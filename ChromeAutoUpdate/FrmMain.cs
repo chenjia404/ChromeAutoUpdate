c95b9bc89ab68b990f143064ca7c21a0d4f94eba
chenjia404 <chenjiablog@gmail.com>
2017-05-17 23:17:30 +0800

    把dll打包进去

--- ChromeAutoUpdate/FrmMain.cs
+++ ChromeAutoUpdate/FrmMain.cs
@@ -14,9 +14,21 @@ namespace ChromeAutoUpdate
     {
         public string[] cmd_args;
 
+        System.Reflection.Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args)
+        {
+            string dllName = args.Name.Contains(",") ? args.Name.Substring(0, args.Name.IndexOf(',')) : args.Name.Replace(".dll", "");
+            dllName = dllName.Replace(".", "_");
+            if (dllName.EndsWith("_resources")) return null;
+            System.Resources.ResourceManager rm = new System.Resources.ResourceManager(GetType().Namespace + ".Properties.Resources", System.Reflection.Assembly.GetExecutingAssembly());
+            byte[] bytes = (byte[])rm.GetObject(dllName);
+            return System.Reflection.Assembly.Load(bytes);
+        }
+
+
         public FrmMain(string[] args)
         {
             this.cmd_args = args;
+            AppDomain.CurrentDomain.AssemblyResolve += new ResolveEventHandler(CurrentDomain_AssemblyResolve);
             InitializeComponent();
         }
 
